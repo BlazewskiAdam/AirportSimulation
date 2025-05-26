2d7e768408c5348b63092055db96d3856c841865
Blazewski Adam <284464@student.pwr.edu.pl>
2025-05-26 17:42:52 +0200

    started 2 classes

---
+++ lotnisko/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Lotnisko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w aplikacji Lotnisko!");
+            // Tutaj można dodać logikę zarządzania operacjami lotniska.
+        }
+    }
+}
